c154d47fa7dc234f8f1178c3c5d437cd32ff37e1
rapiahmadcengeng <111480114+rapiahmadcengeng@users.noreply.github.com>
2022-08-21 22:10:32 -0700

    Add files via upload

---
+++ python4-rafi (1).py
@@ -0,0 +1,16 @@
+print("halo semuanya")
+print("perkenalan")
+nama_saya_adalah=input("nama saya adalah")
+nama_lengkap=input("nama lengkap saya adalah")
+
+a =int(input("masukan nilai a: "))
+b =float(input("masukan nilai b: "))
+c = a + b
+
+print("a =", a)
+print("b =", b)
+print(a, "+","=", c)
+
+x =int(input("masukan suhu celcius"))
+y =(9/5 * x) + 32
+print(x, "celcius= ",y, "fahreheit")
