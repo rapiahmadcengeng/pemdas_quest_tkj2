6f2898e3a096493e8185217d0d984172bd14b1dc
rapiahmadcengeng <111480114+rapiahmadcengeng@users.noreply.github.com>
2022-08-21 22:11:21 -0700

    Add files via upload

---
+++ python2-rapi.py
@@ -0,0 +1,8 @@
+nama_depan="rafi"
+print("nama depan anda adalah",nama_depan)
+
+nama_belakang="ansena"
+print("nama belakang anda adalah",nama_belakang)
+
+nama_lengkap="nama_depan,nama belakang"
+print("nama lengkap anda adalah",nama_depan,nama_belakang)
---
+++ python3-rapi.py
@@ -0,0 +1,19 @@
+nama_depan=input("masukkan nama depan:")
+nama_tengah=input("masukkan nama tengah:")
+nama_belakang=input("masukkan nama belakang:")
+print("nama lengkap saya adalah",nama_depan,nama_tengah,nama_belakang)
+
+a=int(input("masukkan nilai a:"))
+b=float(input("masukkan nilai b:"))
+c=a+b
+
+print("a=",a)
+print("b=",b)
+print(a,"+",b,"=",c)
+
+
+#lengkapi script berikut ini 
+
+x =int(input("masukkan suhu celcius"))
+y = (9/5 * x) + 32
+print(x, "celcius=",y, "fahreheit")
